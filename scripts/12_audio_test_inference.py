0a1dba15fc7c7b5e6603a15966b72ef22aea65f6
Srijan Sati <sati.srijan@tftus.com>
2026-06-12 11:49:10 +0530

    inference using audio file

---
+++ scripts/12_audio_test_inference.py
@@ -0,0 +1,58 @@
+import soundfile as sf
+
+from transformers import Qwen3OmniMoeForConditionalGeneration, Qwen3OmniMoeProcessor
+from qwen_omni_utils import process_mm_info
+
+MODEL_PATH = "./LLaMA-Factory/outputs/stage1_merged"
+# MODEL_PATH = "Qwen/Qwen3-Omni-30B-A3B-Thinking"
+
+model = Qwen3OmniMoeForConditionalGeneration.from_pretrained(
+    MODEL_PATH,
+    dtype="auto",
+    device_map="auto",
+    attn_implementation="flash_attention_2",
+)
+
+processor = Qwen3OmniMoeProcessor.from_pretrained(MODEL_PATH)
+
+conversation = [
+    {
+        "role": "user",
+        "content": [
+            {"type": "audio", "audio": "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen3-Omni/demo/cough.wav"},
+            {"type": "text", "text": "Answer the question asked in the audio. Please answer in hindi."}
+        ],
+    },
+]
+
+# Set whether to use audio in video
+USE_AUDIO_IN_VIDEO = True
+
+# Preparation for inference
+text = processor.apply_chat_template(conversation, add_generation_prompt=True, tokenize=False)
+audios, images, videos = process_mm_info(conversation, use_audio_in_video=USE_AUDIO_IN_VIDEO)
+inputs = processor(text=text, 
+                   audio=audios, 
+                   images=images, 
+                   videos=videos, 
+                   return_tensors="pt", 
+                   padding=True, 
+                   use_audio_in_video=USE_AUDIO_IN_VIDEO)
+inputs = inputs.to(model.device).to(model.dtype)
+
+# Inference: Generation of the output text and audio
+text_ids, audio = model.generate(**inputs, 
+                                 speaker="Ethan", 
+                                 thinker_return_dict_in_generate=True,
+                                 use_audio_in_video=USE_AUDIO_IN_VIDEO)
+
+text = processor.batch_decode(text_ids.sequences[:, inputs["input_ids"].shape[1] :],
+                              skip_special_tokens=True,
+                              clean_up_tokenization_spaces=False)
+print(text)
+if audio is not None:
+    sf.write(
+        "output.wav",
+        audio.reshape(-1).detach().cpu().numpy(),
+        samplerate=24000,
+    )
